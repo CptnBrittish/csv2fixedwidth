fe27de4947cc312a224106328b4ab1c892984ceb
Thomas <18033120@student.uws.edu.au>
2015-02-05 11:58:29 +1100

    Split fixed width conversion into a seperate function and added some more command line options which currently do nothing

--- main.c
+++ main.c
@@ -12,6 +12,101 @@
 #include <sys/stat.h>
 
 FILE *file;
+//switch to convert to fixed width
+int cfw = 0;
+//switch to write to file
+int wf = 0;
+//file path to wrie to
+char fptowrt[PATH_MAX+1];
+
+void * convert_to_fixed_width(char * csv){
+// Find how many collumns are in the file
+  int cnum = 0;
+  int i = 0;
+  char c = csv[i];
+  while((c != '\0') && (c != '\n')){
+    c = csv[i++];
+    //We test all three in case there is only one row of records
+    if(c == ',' || c =='\n' || c == '\0'){
+      cnum++;
+    }
+    if(c == '\n'){
+      c = csv[i++];
+    }
+  }
+
+  int num_columns = cnum;
+  // Create array of ints to hold how long each collums max length is
+  // Add a space after each collumn to make it human readable
+  int clength[cnum];
+  for(int i = cnum; i >= 0; i--){
+    clength[i] = 0;
+  }
+  
+  int col = 0;
+  int rows = 1;
+  cnum = 0;
+  i = 0;
+  c = csv[i];
+  while(c != '\0'){
+    if(c == ',' || c =='\n'){
+      if(col+1 > clength[cnum]){
+	clength[cnum] = col;
+      }
+      col = 0;
+      cnum++;
+    }
+    if(c == '\n'){
+      cnum = 0;
+      col = 0;
+      rows++;
+    }
+    col++;
+    c = csv[i++];
+  }
+
+  // reset all counters
+  i = 0;
+  c = csv[i];
+  cnum = 0;
+  col = 0;
+
+
+  // find complete size of fixed width file and create it
+  int fw_file_size = 0;
+  for(int i = num_columns; i >= 0; i--){
+    fw_file_size += clength[i];
+  }
+  fw_file_size = fw_file_size * rows;
+
+  char * fw = (char *)malloc(sizeof(char)*fw_file_size+1); // The fixed width file to return 
+  int fw_pos = 0;
+  while(c != '\0'){
+    while((c != ',') && (c != '\0') && (c != '\n')){
+      fw[fw_pos] = c;
+      fw_pos++;
+      col++;
+      c = csv[i++];
+    }
+    while(col < clength[cnum]){
+      fw[fw_pos] =  ' ';
+      fw_pos++;
+      col++;
+    }
+    cnum++;
+    col = 0;
+    
+    if(c == '\n'){
+      fw[fw_pos] =  '\n';
+      fw_pos++;
+      cnum = 0;
+    }
+    c = csv[i++];
+  }
+  fw[fw_pos] = '\0';
+
+  return fw;
+}
 
 int main(int argc, char *argv[]){
   int opt;
@@ -21,9 +116,21 @@ int main(int argc, char *argv[]){
     printf("No input\n");
     return -1;
   }
-  while((opt = getopt(argc, argv, "f:")) != -1) {
+  while((opt = getopt(argc, argv, "ocw:f:")) != -1) {
     switch(opt) {
+    case 'c':
+      // We are converting to fixed width
+      cfw = 1;
+      break;
+    case 'o':
+      wf = 1;
+      break;
+    case 'w':
+      // path of file to write to
+      realpath(optarg, fptowrt);
+      break;
     case 'f':
+      //path of file to convert
       realpath(optarg, path);
       
       if((file = fopen(path, "r")) == NULL){
@@ -41,82 +148,22 @@ int main(int argc, char *argv[]){
   // We now want the file in a array so we can move back and forth through it
   struct stat sb;
   stat(path, &sb);
-  int array[sb.st_size];
+  char array[sb.st_size];
   int array_size = 0;
   int i = 0;
   int c = 0;
   while((c = fgetc(file)) != EOF){
     array[i] = c;
     i++;
-    array_size++;
   }
-  array[i++] = EOF;
-  array_size++;
+  array[i++] = '\0';
 
-  // Find how many collumns are in the file
-  int cnum = 0;
-  i = 0;
-  c = array[i];
-  while((c != EOF) && (c != '\n')){
-    c = array[i++];
-    //We test all three in case there is only one row of records
-    if(c == ',' || c =='\n' || c == EOF){
-      cnum++;
-    }
+  char * fw = convert_to_fixed_width(array);
+  if(wf){
 
-  }
-
-  // Create array of ints to hold how long each collums max length is
-  // Add a space after each collumn to make it human readable
-  int clength[cnum];
-  for(int i = cnum; i >= 0; i--){
-    clength[i] = 0;
-  }
-  int col = 0;
-  cnum = 0;
-  i = 0;
-  c = array[i];
-  while(i < array_size){
-    if(c == ',' || c =='\n'){
-      if(col+1 > clength[cnum]){
-	clength[cnum] = col;
-      }
-      col = 0;
-      cnum++;
+  } else {
+    fprintf(stdout, "%s", fw);
+    fprintf(stdout, "\n"); // add a newline before exiting
     }
-    if(c == '\n'){
-      cnum = 0;
-      col = 0;
-    }
-    col++;
-    c = array[i++];
-  }
-
-  i = 0;
-  c = array[i];
-  cnum = 0;
-  col = 0;
-  for(int i = 0; i <= 2; i++){
-  }
-  while(i < array_size){
-    while((c != ',') && (i < array_size) && (c != '\n')){
-      fprintf(stdout, "%c", c);
-      col++;
-      c = array[i++];
-    }
-    while(col < clength[cnum]){
-      fprintf(stdout, " ");
-      col++;
-    }
-    cnum++;
-    col = 0;
-    
-    if(c == '\n'){
-      fprintf(stdout, "\n");
-      cnum = 0;
-    }
-    c = array[i++];
-  }
-  fprintf(stdout, "\n");  
   return 0;
 }
